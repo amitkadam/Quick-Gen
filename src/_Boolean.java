30c4f95065a5b8441b90e597740b8521025f1512
Mayur Jadhav <mayur@helpshift.com>
2015-02-22 01:41:17 +0530

    Add class to generate boolean data.

---
+++ src/_Boolean.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class _Boolean {
+	boolean[] defaultBooleanArray;
+	
+	/** Generate random boolean value **/
+	public boolean gen() {
+		return new Random().nextBoolean();
+	}
+	
+	/** Generate random boolean array of given limit . */
+	public boolean[] gen(int limit)
+	{
+		defaultBooleanArray = new boolean[limit];
+		Random randomGenerator = new Random();
+
+		//note a single Random object is reused here
+		for (int idx = 1; idx <= limit; ++idx){			
+			defaultBooleanArray[idx-1] = randomGenerator.nextBoolean();
+		}
+		return defaultBooleanArray;
+	}
+}
